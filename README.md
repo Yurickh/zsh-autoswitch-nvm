bee11d5148ae81e1cdb05affa8f27713701a9df4
Yurick <yurick.hauschild@gmail.com>
2018-12-03 11:16:46 -0200

    🎉  Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Autoswitch Node nvm version
+
+_zsh-autoswitch-nvm_ is a simple package that switches your nvm version when you cd into a directory with a package.json.
+
+## Caveats
+
+- This package won't check for `.nvmrc` files, since nvm checks for it automatically already.
+- You'll need the [semver](https://www.npmjs.com/package/semver) package installed globally.
+
+## Installing
+
+By hand
+
+```bash
+git clone https://github.com/Yurickh/zsh-autoswitch-nvm ~/.dotfiles/lib/zsh-autoswitch-nvm
+echo 'source ~/.dotfiles/lib/zsh-autoswitch-nvm' >> ~/.zshrc
+```
+
+I'm not currently aware of how to link this package to any package manager, so I don't know if you can use them to install it.
+
+## Deactivating
+
+Set the `DISABLE_AUTOSWITCH_VENV` variable to temporarilly disable version switching when loading a new terminal window.
---
+++ autoswitch_nvm.plugin.zsh
@@ -0,0 +1,34 @@
+function load_nvmrc() {
+  if [[ -f package.json && -r package.json ]]; then
+    echo "Checking for installed npm version..."
+    range=$(node -p "(require('./package.json').engines || {}).node || 'none'")
+
+    if [ $range = "none" ]; then
+      echo "No node version specified in package.json."
+    else
+      version=$(semver -r $range $(nvm ls --no-colors | grep -v ">" | xargs | tr -d '*') | tail -n 1)
+      nvm use $version > /dev/null 2>&1
+
+      if [ $? != 0 ]; then
+        available=$(semver -r $range $(nvm ls-remote --no-colors | grep -oE '.*?v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | xargs ) | tail -n 1)
+        echo "Couldn't install version $version, downloading version $available"
+        nvm install $available
+
+        if [ $? != 0 ]; then
+          echo "Couldn't install version $available"
+        fi
+      fi
+    fi
+  elif [[ $(nvm version) != $(nvm version default)  ]]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+
+if [[ -z "$DISABLE_AUTOSWITCH_VENV" ]]; then
+    autoload -Uz add-zsh-hook
+    add-zsh-hook -D chpwd load_nvmrc
+    add-zsh-hook chpwd load_nvmrc
+
+    load_nvmrc
+fi
