87c71eb8103f0e66b89344fb7c49c98cc2cc45db
Yurick <yurick.hauschild@gmail.com>
2019-10-28 08:19:57 +0100

    Add support to .node-version

--- autoswitch_nvm.plugin.zsh
+++ autoswitch_nvm.plugin.zsh
@@ -1,28 +1,39 @@
-function load_nvmrc() {
-  if [[ -f package.json && -r package.json ]]; then
-    echo "Checking for installed npm version..."
-    range=$(node -p "(require('./package.json').engines || {}).node || 'none'")
+function read_node_range() {
+  range="none"
 
-    if [ $range = "none" ]; then
-      echo "No node version specified in package.json."
-      nvm use default
-    else
-      version=$(semver -r $range $(nvm ls --no-colors | grep -v " ->" | xargs | tr -d '*\->') | tail -n 1)
-      nvm use $version > /dev/null 2>&1
+  if [[ -f .node-version && -r .node-version ]]; then
+    range=$(cat .node-version)
+  fi
+
+  if [ $range = "none" ]; then
+    if [[ -f package.json && -r package.json ]]; then
+      range=$(node -p "(require('./package.json').engines || {}).node || 'none'")
+    fi
+  fi
+
+  echo $range
+}
+
+function load_nvmrc() {
+  echo "Checking for installed npm version..."
+  range=`read_node_range`
+
+  if [ $range = "none" ]; then
+    echo "No node version configured in workspace."
+    nvm use default
+  else
+    version=$(semver -r $range $(nvm ls --no-colors | grep -v " ->" | xargs | tr -d '*\->') | tail -n 1)
+    nvm use $version > /dev/null 2>&1
+
+    if [ $? != 0 ]; then
+      available=$(semver -r $range $(nvm ls-remote --no-colors | grep -oE '.*?v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | xargs ) | tail -n 1)
+      echo "Couldn't find local version in range $range, downloading version $available"
+      nvm install $available
 
       if [ $? != 0 ]; then
-        available=$(semver -r $range $(nvm ls-remote --no-colors | grep -oE '.*?v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | xargs ) | tail -n 1)
-        echo "Couldn't find local version in range $range, downloading version $available"
-        nvm install $available
-
-        if [ $? != 0 ]; then
-          echo "Couldn't install version $available"
-        fi
+        echo "Couldn't install version $available"
       fi
     fi
-  elif [[ $(nvm version) != $(nvm version default)  ]]; then
-    echo "Reverting to nvm default version"
-    nvm use default
   fi
 }
 
